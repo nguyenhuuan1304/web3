9add532eecc9f99721145c225f0c7a5b80f1900c
An Nguyen <nhuuan@tma.com.vn>
2024-05-21 10:46:44 +0700

    first commit

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract MyContract {
-    constructor() {}
+contract Counter {
+    uint256 public count = 0;
+
+    function increment() public {
+        count += 1;
+    }
+
+    function decrement() public {
+        require(count > 0, "Count cannot be less than 0");
+        count -= 1;
+    }
+
+    function getCount() public view return (uint256) {
+       return count;
+    }
 }
